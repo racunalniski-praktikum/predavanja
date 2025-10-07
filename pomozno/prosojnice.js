a2df67e4188512f04c070abdc285100253e012f0
Matija Pretnar <matija@pretnar.info>
2025-10-07 11:08:56 +0200

    Ustrezno naloži JavaScript kodo

--- pomozno/prosojnice.js
+++ pomozno/prosojnice.js
@@ -1,3 +1,20 @@
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
+
+// Initialize Mermaid but don't render diagrams on start
+mermaid.initialize({
+    startOnLoad: false,
+    cloneCssStyles: false,
+    gitGraph: {
+        showBranches: false
+    }
+});
+
+function initMermaid(s) {
+    const elements = document.querySelectorAll(".remark-slide")[s.getSlideIndex()]
+        ?.querySelectorAll('.mermaid-diagram p:not([data-processed="true"])');
+    if (elements) mermaid.init(undefined, elements);
+}
+
 var jQuerySrc = document.createElement('script');
 jQuerySrc.src = 'https://code.jquery.com/jquery-1.12.4.min.js';
 jQuerySrc.type = 'text/javascript';
@@ -18,17 +35,21 @@ jQuerySrc.onload = function() {
 document.head.appendChild(jQuerySrc);
 
 var remarkSrc = document.createElement('script');
-var slideshow;
 remarkSrc.src = 'https://remarkjs.com/downloads/remark-latest.min.js';
 remarkSrc.type = 'text/javascript';
 remarkSrc.onload = function () {
-  slideshow = remark.create({
+  var slideshow = remark.create({
     highlightLines: true,
     highlightLanguage: 'ocaml',
     highlightStyle: 'github',
     countIncrementalSlides: false,
     ratio: '16:9'
   });          
+  // Render diagrams immediately on the current slide
+  initMermaid(slideshow.getSlides()[slideshow.getCurrentSlideIndex()]);
+
+  // Render diagrams when there is a navigation to another slide
+  slideshow.on("afterShowSlide", initMermaid);
 };
 document.head.appendChild(remarkSrc);
 
@@ -36,4 +57,3 @@ var mathJaxSrc = document.createElement('script');
 mathJaxSrc.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
 mathJaxSrc.type = 'text/javascript';
 document.head.appendChild(mathJaxSrc);
-
